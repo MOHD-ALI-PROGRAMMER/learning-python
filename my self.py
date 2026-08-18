6865a820c0aab8622cd9ca723c092c8197620e93
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-18 21:50:20 +0530

    read it myself

---
+++ my self.py
@@ -0,0 +1,7 @@
+name:str="mohd ali"
+age:int="17"
+height:float="5.8"
+colour:str="light brown"
+is_iq_present:bool="false"
+mohdali=(name,age,height,colour,is_iq_present)
+print("mohdali=",mohdali)
