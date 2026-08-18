9a06705e1985800c2abb5c7300df3e63aedf7ccc
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-18 21:35:16 +0530

    volume of a cuboid

---
+++ area of l-shaped.py
@@ -0,0 +1,28 @@
+width upp. rectag:int=7
+length upp. rectag:int=4
+area of upp. rectag=(2*(width upp. rectag+length upp. rectag))
+print(area of upp. rectag)
+width lower rectag:int=12
+length lower rectag:int=4
+area of lower rectag=(2*(width lower rectag+length lower rectag))
+print(rea of lower rectag)
+total area of a shape=(area of upp. rectag+area of lower rectag)
+print("total area of a shape=",total area of a shape,"meter square")width upp. rectag:int=7
+length upp. rectag:int=4
+area of upp. rectag=(2*(width upp. rectag+length upp. rectag))
+print(area of upp. rectag)
+width lower rectag:int=12
+length lower rectag:int=4
+area of lower rectag=(2*(width lower rectag+length lower rectag))
+print(rea of lower rectag)
+total area of a shape=(area of upp. rectag+area of lower rectag)
+print("total area of a shape=",total area of a shape,"meter square")width upp. rectag:int=7
+length upp. rectag:int=4
+area of upp. rectag=(2*(width upp. rectag+length upp. rectag))
+print(area of upp. rectag)
+width lower rectag:int=12
+length lower rectag:int=4
+area of lower rectag=(2*(width lower rectag+length lower rectag))
+print(rea of lower rectag)
+total area of a shape=(area of upp. rectag+area of lower rectag)
+print("total area of a shape=",total area of a shape,"meter square")
---
+++ shape area.py
@@ -0,0 +1,10 @@
+AB:int=7
+AF:int=4
+ upper area=((2*AB)+(2*AF))
+ print(upper area)
+ED:int=4
+DC:int=12
+lower area=((2*ED)+(2*DC))
+print(lower area)
+total area=(upper area+lower area)
+print("total area=",total area,"meter square")
