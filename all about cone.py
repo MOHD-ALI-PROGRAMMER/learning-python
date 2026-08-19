cba140ffd9cbb033e352c74f9e3db7db11b8162f
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-19 22:45:11 +0530

    #improve mistakes

--- all about cone.py
+++ all about cone.py
@@ -1,12 +1,14 @@
 height:int=2
 radius:int=2
+
 slant_height=((height**2+radius**2)**0.5)
-print(slant_height)
+print("slant_height=",slant_height)
+
 curved_area=(3.14*radius*slant_height)
-print(curved_area,"meter square")
+print("curved_area=",curved_area,"meter square")
+
 volume=(1/3*(3.14)*radius**2*height)
-print(volume,"meter cube")
+print("volume=",volume,"meter cube")
+
 surface_area=(3.14*radius*(radius+slant_height))
-print(surface_area,"meter square")
-all_measures:str=("slant_height=",slant_height,"curved_area=",curved_area,"volume=",volume,"surface_area=",surface_area)
-print("all_measures=",all_measures,)
+print("surafce_area=",surface_area,"meter square")
