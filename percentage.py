11ae1ccd7d5a1d086fa66da2558605f8d80cb539
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-20 19:53:47 +0530

    check percentage

--- percentage.py
+++ percentage.py
@@ -3,9 +3,9 @@ name= input("enter your name ")
 maths= float(input("maths enter your marks"))
 physics= float(input("physics enter your marks"))
 chemistry= float(input("chemistry enter your name"))
-english= float(input("english enter your marks"))
+english= float(input("english enter your marks")) 
 total= maths+physics+chemistry+english
-percentage= total/4
+(percentage)= (total/320)*100
 
 print("name:",name)
 print("total:",total)
