6dbcc683caaea91bbc5a41e9a00c4b544bd37229
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-21 00:14:29 +0530

    confirm your age

---
+++ age _calculator.py
@@ -0,0 +1,8 @@
+name= str(input("enter your name"))
+birth_year= int(input("enter your birth year"))
+
+current_year= 2026
+age= (current_year - birth_year)
+
+print("assalam mualaikum",name)
+print("you are",age,"old")
