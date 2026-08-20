901ca218adfcaeaf98343a2d311de8649c6c2ee1
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-20 18:50:11 +0530

    #find your percentage

---
+++ percentage.py
@@ -0,0 +1,12 @@
+name= input("enter your name ")
+
+maths= float(input("maths enter your marks"))
+physics= float(input("physics enter your marks"))
+chemistry= float(input("chemistry enter your name"))
+english= float(input("english enter your marks"))
+total= maths+physics+chemistry+english
+percentage= total/4
+
+print("name:",name)
+print("total:",total)
+print("percentage:",percentage,"%")
