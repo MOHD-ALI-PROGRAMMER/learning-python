6e1d6c9a8271d5d17ec5961faf651c1f6000001b
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-22 00:06:59 +0530

    check temperature units

--- temperature-converter .py
+++ temperature-converter .py
@@ -1,16 +1,39 @@
-number = float(input("ENTER YOUR TEMPERATURE: "))
+temperature= float(input("ENTER YOUR TEMPERATURE: "))
 
 print("1. fahrenheit → celsius")
 print("2. celsius → fahrenheit")
+print("3. fahrenheit → kelvin")
+print("4. kelvin → fahrenheit")
+print("5. celsius → kelvin")
+print("6. kelvin → celsius")
 
 choice = int(input("ENTER YOUR OPERATION:"))
 
 if choice == 1:
-    celsius = (number-32)*5/9
+    celsius = (temperature-32)*5/9
     print("YOUR TEMP. IN CELSIUS IS:", celsius,"°C")
 
 elif choice == 2:
-    fahrenheit = (number*1.8)+32
+    fahrenheit = (temperature*1.8)+32
     print("YOUR TEMP.IN FAHRENHEIT IS:", fahrenheit,"°F")
 
-else:("INVALID OPERATION SIR")
+elif choice ==3:
+    kelvin = ((temperature -32)*5/9)+273.15
+    print("YOUR TEMP. IN KELVIN IS:", kelvin,"K")
+
+elif choice == 4:
+    fahrenheit = ((temperature-273.15)*9/5)+32
+    print("YOUR TEMP. IN FAHRENHEIT IS:",fahrenheit,"°F")
+
+elif choice == 5:
+    kelvin = temperature + 273.15
+    print("YOUR TEMP. IN KELVIN IS:",kelvin,"K")
+
+elif choice == 6:
+    celsius = temperature - 273.15
+    print("YOUR TEMP. IN CELSIUS IS:", celsius,"°C")
+
+else:
+    print("INVALID OPERATION SIR ")
+    
+    print("TRY AGAIN")
