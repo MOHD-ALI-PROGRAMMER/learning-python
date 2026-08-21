f93f43b08f4ed1d9de5bfe44a275bc71bceca941
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-21 20:37:02 +0530

    play boy

---
+++ mad_lib.py
@@ -0,0 +1,9 @@
+name = input("enter name: ")
+animal = input("enter aniaml name: ")
+objective = input("enter your objctive: ")
+number = input("enter number: ")
+
+print(f" Million years ago {name} was seing some {animal} outside the toilet and {name} think to suck it suddenly {animal} attack on him/her and doing sex. {name} was surprised.")
+print(f"{animal} playing with {objective} and break it, then {name} come and again doing sex with {animal} during {number} minutes and after both get fatigue.")
+print(f  {animal} gets scared for seeing {name} in well form  and {name} wants sex again and {animal} runs out from there.")
+print(f"in the end {name} was safe for {animal} but we saw (animal) was safe from {name}")
