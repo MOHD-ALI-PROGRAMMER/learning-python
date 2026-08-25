2d48e52abf0e2ae2f2c55615843e8ae199e70d7c
MOHD-ALI-PROGRAMMER <sheikh.mohd.ali2010@gmail.com>
2026-08-25 23:42:26 +0530

    modified calculator

--- calculator.py
+++ calculator.py
@@ -4,13 +4,13 @@ number2= float(input("ENTER YOUR NUMBER: "))
 print("1. multiply")
 print("2. divide")
 print("3. add")
-print("4. substract")
+print("4. subtract")
 
 choice = int(input("choose Your Operation: "))
 
 if choice == 1:
-    multiply = number1 * number2
-    print("YOUR ANSWER IS:", multiply)
+     multiply = number1 * number2
+     print("YOUR ANSWER IS:", multiply)
 
 elif choice == 2:
      divide = number1 / number2
@@ -19,10 +19,10 @@ elif choice == 2:
 elif choice == 3:
      add = number1 + number2
      print("YOUR ANSWER IS:", add)
- 
+
 elif choice == 4:
-     substract = number1 - number2
-     print("YOUR ANSWER IS:", substarct)
+     subtract = number1 - number2
+     print("YOUR ANSWER IS:", subtract)
 
 else:
      print("INVALID OPERATION SIR")
